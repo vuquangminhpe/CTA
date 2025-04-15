304b5658fbda6ca4d487e3ec7e1261981622fe72
vuquangminhpe <minhvqhe176726@fpt.edu.vn>
2025-04-15 14:49:25 +0700

    fix bug

--- client/src/constants/config.ts
+++ client/src/constants/config.ts
@@ -1,5 +1,5 @@
 const configBase = {
-  baseURL: import.meta.env.VITE_PROCESS === 'development' ? 'http://localhost:5000' : 'https://cta-iota.vercel.app'
+  baseURL: import.meta.env.VITE_PROCESS !== 'development' ? 'http://localhost:5000' : 'https://cta-iota.vercel.app'
 }
 
 export default configBase
