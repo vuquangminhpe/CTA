8048d03953227fc59ee88258cea4848ac1bd699c
vuquangminhpe <minhvqhe176726@fpt.edu.vn>
2025-04-15 14:49:14 +0700

    update code

--- client/src/constants/config.ts
+++ client/src/constants/config.ts
@@ -1,5 +1,5 @@
 const configBase = {
-  baseURL: 'http://localhost:5000'
+  baseURL: import.meta.env.VITE_PROCESS === 'development' ? 'http://localhost:5000' : 'https://cta-iota.vercel.app'
 }
 
 export default configBase
