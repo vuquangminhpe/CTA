cc7a3e681e5eb901d73c942002de6ab3970e8c0a
vuquangminhpe <minhvqhe176726@fpt.edu.vn>
2025-06-06 01:39:09 +0700

    fix link

--- client/src/constants/config.ts
+++ client/src/constants/config.ts
@@ -1,5 +1,5 @@
 const configBase = {
-  baseURL: import.meta.env.VITE_PROCESS !== 'development' ? 'http://localhost:5000' : 'http://139.59.250.30/'
+  baseURL: import.meta.env.VITE_PROCESS !== 'development' ? 'http://localhost:5000' : 'https://139.59.250.30/'
 }
 
 export default configBase
